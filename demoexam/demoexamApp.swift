25b57e82c4d6d39284cc0f9db3a0e9b1952fd46d
0FlapJack0 <jacktorov@yandex.ru>
2024-10-11 19:37:02 -0700

    Initial Commit

---
+++ demoexam/demoexamApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoexamApp.swift
+//  demoexam
+//
+//  Created by user on 11.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct demoexamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
